3ee83e9fc0cee26b0d37383da8bc2c1dee553bc3
dev <itinera-support@famifed.be>
2019-05-02 13:35:34 +0200

    initial project

---
+++ src/main/java/be/vutg/demoaxon/DemoAxonApplication.java
@@ -0,0 +1,13 @@
+package be.vutg.demoaxon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAxonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAxonApplication.class, args);
+    }
+
+}
